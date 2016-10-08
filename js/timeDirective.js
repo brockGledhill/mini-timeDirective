879b1092259ca27d2eebe24fe67b621950711633
Brock Gledhill <brock_2047@hotmail.com>
2016-10-08 10:23:11 -0600

    update

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -4,8 +4,8 @@ angular.module('timeApp').directive('showTime', function() {
         restrict: 'E',
         template: '<div> The current time is {{time}} </div>',
         link: function($scope, element, attrs) {
-            var currentTime = new Date();
-            $scope.time = currentTime.toString();
+            $scope.time = new Date().toString();
+
         },
     };
 
